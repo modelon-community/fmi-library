f7a7ae0f0201a1025f1c0e7e8419f7ceafb05bdc
chria <chria@7ae51c65-13d5-4921-9d80-6511ddd1419c>
2013-06-20 07:27:35 +0000

    Reverted the name of the nominal function. Related to ticket:2931

--- src/Import/include/FMI2/fmi2_import_capi.h
+++ src/Import/include/FMI2/fmi2_import_capi.h
@@ -411,7 +411,7 @@ FMILIB_EXPORT fmi2_status_t fmi2_import_get_continuous_states(fmi2_import_t* fmu
  * @param nx Number of nominal values.
  * @return FMI status.
  */
-FMILIB_EXPORT fmi2_status_t fmi2_import_get_nominals_of_continuous_states(fmi2_import_t* fmu, fmi2_real_t x_nominal[], size_t nx);
+FMILIB_EXPORT fmi2_status_t fmi2_import_get_nominal_continuous_states(fmi2_import_t* fmu, fmi2_real_t x_nominal[], size_t nx);
 
 /**
  * \brief Wrapper for the FMI function fmiTerminate(...) 

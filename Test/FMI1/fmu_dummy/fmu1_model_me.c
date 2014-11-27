ee9a5ec140dc5f78ee1e303d7925111541a892b8
ziegler <ziegler@7ae51c65-13d5-4921-9d80-6511ddd1419c>
2014-11-27 13:15:40 +0000

    #4087: new branch for FMU Examples

--- Test/FMI1/fmu_dummy/fmu1_model_me.c
+++ Test/FMI1/fmu_dummy/fmu1_model_me.c
@@ -4,8 +4,6 @@ Copyright (C) 2012 Modelon AB
 This program is free software: you can redistribute it and/or modify
 it under the terms of the BSD style license.
 
-the Free Software Foundation, version 3 of the License.
-
 This program is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the

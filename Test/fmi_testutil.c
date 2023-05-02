modelon-community/fmi-library

commit ca54670e4853983cc57cd24f9a0d11444934d0f5

Update SDK for Jenkins (#39)
Update SDK for Jenkins

Jenkinsfile.groovy
@@ -1,27 +1,27 @@
 // Node requirements:
 //  VisualStudio2017: C compiler
-//  OCT-SDK-1.4: CMake, msys
+//  OCT-SDK-1.5.4: CMake, msys
 //  docker: docker
 
 def Configs = [
     'win64': [
         name: 'win64',
         os: 'windows',
-        node: 'VisualStudio2017 && OCT-SDK-1.5',
+        node: 'VisualStudio2017 && OCT-SDK-1.5.4',
         target_install: 'install',
         target_test: 'test'
     ],
     'win64_static_runtime': [
         name: 'win64_static_runtime',
         os: 'windows',
-        node: 'VisualStudio2017 && OCT-SDK-1.5',
+        node: 'VisualStudio2017 && OCT-SDK-1.5.4',
         target_install: 'install',
         target_test: 'test'
     ],
     'mingw_w64': [
         name: 'mingw_w64',
         os: 'windows',
-        node: 'OCT-SDK-1.5',
+        node: 'OCT-SDK-1.5.4',
         target_install: 'install',
         target_test: 'test'
     ], 

build/setenv.bat
@@ -1,7 +1,2 @@
-:: DARWIN_SDK_HOME: OCT's SDK for darwin
-
-:: Add msys to path
-set PATH=%DARWIN_SDK_HOME%\\MinGW\\msys\\1.0\\bin;%PATH%
-:: Add cmake to path
-set PATH=%DARWIN_SDK_HOME%\\CMake\\bin;%PATH%
-
+:: Add cmake and msys to PATH
+set PATH=%OCT_SDK_HOME%\\CMake\\bin;%OCT_SDK_HOME%\\MinGW\\msys\\1.0\\bin;%PATH%

commit f035424dd968f1ca881038be071b3b4bb17be319

Fix memory leaks introduced in #39 and make Jenkins report them (#41)

Jenkinsfile.groovy
@@ -59,7 +59,6 @@ Configs.each { conf_entry ->
 
             stage("Checkout: ${conf.name}") {
                 checkout scm
-                fixFilePermissions(conf.os)
             }
 
             stage("Clean artifacts: ${conf.name}") {
@@ -87,7 +86,6 @@ tasks[conf.name] = {
 
         stage("Checkout: ${conf.name}") {
             checkout scm
-            fixFilePermissions(conf.os)
         }
 
         stage("Clean artifacts: ${conf.name}") {
@@ -163,7 +161,7 @@ def test(conf, testLogDir) {
         error(message: "Invalid config operating system: ${conf.os}")
     }
     if (res) {
-        setBuildStatus('UNSTABLE', "Test failure. Exit code from ctest: ${res}")
+        setBuildStatus('UNSTABLE', "Test failure. Exit code: ${res}")
     }
 
     dir(testLogDir) {
@@ -182,19 +180,4 @@ def test(conf, testLogDir) {
 def setBuildStatus(status, msg) {
     currentBuild.result = status
     println("Build result manually set to ${status}. Reason:\n${msg}")
-}
-
-def fixFilePermissions(os) {
-    if (os == 'linux') {
-        sh """
-            # Allow internal docker script to run
-            chmod a+x build/docker/build.sh
-
-            # Allow copying artifacts to host
-            chmod 777 .
-
-            # All the other scripts we need to run...
-            chmod a+x Test/scripts/verify_memcheck_logs.sh
-        """
-    }
 }

build/docker/Makefile
@@ -1,11 +1,6 @@
 include $(CONFIG_FILE)
 
-# Use this file to build FMI Library on docker.
-# Overview of what happens:
-# 1. An image that contains the current project is created and started
-# 2. The build/docker/build.sh script is called from the container
-# 3. This in turn calls the Makefile in project root
-# 4. The build artifacts are copied to the mounted project root (in host)
+# This file builds FMI Library on docker.
 #
 # NOTE: Working directory when calling this file must be project root.
 
@@ -14,7 +9,7 @@ IMG_TAG=fmil:test
 .PHONY: docker_image install test documentation docker_run_debug
 
 docker_image:
-	docker build -f build/docker/$(DOCKERFILE_NAME) -t $(IMG_TAG) --no-cache . # --no-cache ensures that we get the latest FMIL files
+	docker build -f build/docker/$(DOCKERFILE_NAME) -t $(IMG_TAG) .
 
 install test documentation: docker_image
 	docker run \
@@ -22,5 +17,4 @@ install test documentation: docker_image
 		-u baseuser \
 		-v /$(CURDIR):$(MOUNTED_DIR) \
 		$(IMG_TAG) \
-		//bin/bash -c "$(MOUNTED_DIR)/build/docker/build.sh \
-			$@ $(BUILD_DIR) $(INSTALL_DIR) $(CONFIG)"
+		//bin/bash -c "make $@ CONFIG_FILE=./build/config/$(CONFIG)"

build/docker/build.sh
@@ -1,25 +1,0 @@
-#!/bin/bash
-#
-# This script is only called within the docker container.
-# This script wraps the top-level 'make' command, and ensures that the host
-# user can access the build artifacts.
-#
-# This script must be called from the top-level of the repo.
-
-set -eu -o pipefail
-
-TARGET=$1
-BUILD_DIR=$2
-INSTALL_DIR=$3
-CONFIG=$4
-
-# Call the top-level Makefile
-if [[ $TARGET == 'test' ]]; then
-    # Allow the tests to fail, so we can get the test log
-    make $TARGET CONFIG_FILE=./build/config/$CONFIG || true
-else
-    make $TARGET CONFIG_FILE=./build/config/$CONFIG
-fi
-
-# Build dir needs chmod since it contains the test log
-chmod -R a+rwx $INSTALL_DIR $BUILD_DIR

src/XML/src/FMI3/fmi3_xml_variable.c
@@ -875,6 +875,11 @@ void fmi3_xml_variable_free_internals(jm_callbacks* callbacks, fmi3_xml_variable
             callbacks->free(fmi3_xml_get_variable_start_array(var));
         }
         callbacks->free(var->dimensionsArray);
+        var->dimensionsArray = NULL;
+    }
+    if (var->clocks) {
+        jm_vector_free(fmi3_value_reference_t)(var->clocks);
+        var->clocks = NULL;        
     }
 }
 

Test/FMI3/fmi3_import_variable_test.cpp
@@ -324,6 +324,8 @@ TEST_CASE("Invalid Binary variable - non-hexadecimal char first in byte tuple", 
 
     const char* errMsg = fmi3_import_get_last_error(xml);
     REQUIRE(strcmp(errMsg, "String is not hexadecimal: gf") == 0);
+
+    fmi3_import_free(xml);
 }
 
 TEST_CASE("Invalid Binary variable - non-hexadecimal char second in byte tuple", "[xml_variables]") {
@@ -334,4 +336,6 @@ TEST_CASE("Invalid Binary variable - non-hexadecimal char second in byte tuple",
 
     const char* errMsg = fmi3_import_get_last_error(xml);
     REQUIRE(strcmp(errMsg, "String is not hexadecimal: FG") == 0);
+
+    fmi3_import_free(xml);
 }

Test/fmi_testutil.c
@@ -23,13 +23,18 @@ void fmi_testutil_enter_breakpoint()
 fmi3_import_t* fmi3_testutil_parse_xml(const char* xmldir) {
     jm_callbacks* cb = jm_get_default_callbacks();
     fmi_import_context_t* ctx = fmi_import_allocate_context(cb);
-    ASSERT_MSG(ctx != NULL, "Context was NULL");
+    if (!ctx) {
+        printf("testutil: Context was NULL\n");
+        return NULL;
+    }
 
     fmi3_import_t* xml;
     xml = fmi3_import_parse_xml(ctx, xmldir, NULL);
-    ASSERT_MSG(xml != NULL, "Failed to parse XML");
-
     fmi_import_free_context(ctx);
+    if (!xml) {
+        printf("testutil: Failed to parse XML\n");
+        return NULL;
+    }
 
     return xml;
 }

Test/fmi_testutil.h
@@ -51,7 +51,7 @@ char* concat(char *s1, char *s2);
 #else /* __FUNCTION__ magic variable won't be available */
 #define PRINT_FAILURE(...)                                                  \
     do {                                                                    \
-        printf("  test FAILED at line %d: ", __LINE__);                     \
+        printf("  Assertion FAILED at line %d: ", __LINE__);                \
         printf(__VA_ARGS__);                                                \
         printf("\n");                                                       \
     } while (0)

Test/scripts/verify_memcheck_logs.sh
@@ -32,7 +32,7 @@ logdir="$1"
 memcheck_logs="$(find "$logdir" -not -empty -name "MemoryChecker.*.log")"
 
 if [[ -n "$memcheck_logs" ]]; then
-    err "memcheck gave warnings on tests: $memcheck_logs"
+    err "memcheck problems: $memcheck_logs"
     exit 1
 fi
 